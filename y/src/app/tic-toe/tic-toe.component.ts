9ebebcc549b9a7169eb1129656ee9277adf0f39c
tulasi1356 <botchatulasi1356@gmail.com>
2021-05-21 21:10:34 +0530

    tic-tao completed

--- y/src/app/tic-toe/tic-toe.component.ts
+++ y/src/app/tic-toe/tic-toe.component.ts
@@ -9,6 +9,7 @@ export class TicToeComponent implements OnInit {
   public player1="1";
   public player2="2";
   public start=true;
+  public gameend=false;
   public multi:number[][] = [[0,0,0],[0,0,0],[0,0,0]]  
   constructor() {
     console.log(this.multi)
@@ -17,30 +18,49 @@ export class TicToeComponent implements OnInit {
   }
   public player: any;
   public count=0
-  action(x:any,y:any) {
-    if(this.start)
-    this.player=this.player1
-    else
-    this.player=this.player2
-    this.multi[x][y]=this.player
-    console.log('multi',this.multi)
-    this.start=!this.start
-    this.count++;
-    if(this.count>=3) {
-      for (let i = 0; i < 3; i++) {
-        if((this.multi[i][0]==this.multi[i][1]) && (this.multi[i][2]==this.multi[i][1]) && (this.multi[i][0]==this.multi[i][2]) && this.multi[i][0]!=0) {
-          console.log("winner row",this.player);
-        }
+  public color:any;
+  public colorarray=['','','','','','','','','']
+  action(x:any,y:any,z:any) {
+
+    if(this.multi[x][y]==0) {
+      if(this.start) {
+        this.player=this.player1
+        this.colorarray[z]="pink"
       }
-      for (let i = 0; i < 3; i++) {
-        if((this.multi[0][i]==this.multi[1][i]) && (this.multi[0][i]==this.multi[2][i]) && (this.multi[1][i]==this.multi[2][i]) && this.multi[0][i]!=0) {
-          console.log("winner col",this.player);
-        }
+     
+      else {
+  
+        this.player=this.player2
+        this.colorarray[z]="blue"
+  
       }
-      if((this.multi[0][0]==this.multi[1][1]) && (this.multi[0][0]==this.multi[2][2]) && (this.multi[1][1]==this.multi[2][2]) && this.multi[0][0]!=0 && this.multi[1][1]!=0 && this.multi[2][2]!=0) {
-        console.log("winner diag",this.player);
+      this.multi[x][y]=this.player
+
+      console.log('multi',this.multi)
+      this.start=!this.start
+      this.count++;
+      if(this.count>=3) {
+        for (let i = 0; i < 3; i++) {
+          if((this.multi[i][0]==this.multi[i][1]) && (this.multi[i][2]==this.multi[i][1]) && (this.multi[i][0]==this.multi[i][2]) && this.multi[i][0]!=0) {
+            // console.log("winner row",this.player);
+            this.gameend=true
+          }
+        }
+        for (let i = 0; i < 3; i++) {
+          if((this.multi[0][i]==this.multi[1][i]) && (this.multi[0][i]==this.multi[2][i]) && (this.multi[1][i]==this.multi[2][i]) && this.multi[0][i]!=0) {
+            // console.log("winner col",this.player);
+            this.gameend=true
+          }
+        }
+        if((this.multi[0][0]==this.multi[1][1]) && (this.multi[0][0]==this.multi[2][2]) && (this.multi[1][1]==this.multi[2][2]) && this.multi[0][0]!=0 && this.multi[1][1]!=0 && this.multi[2][2]!=0) {
+          // console.log("winner diag",this.player);
+          this.gameend=true
+        }
       }
     }
+
+ 
+   
 
     
   }
