2da33a431b8ff2a7c8640dfab9b55e5d13ff96eb
tulasi1356 <botchatulasi1356@gmail.com>
2021-05-21 18:52:21 +0530

    logic correct

--- y/src/app/app-routing.module.ts
+++ y/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { TicToeComponent } from './tic-toe/tic-toe.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '', redirectTo: 'tic', pathMatch: 'full'},
   {path: 'home', component:HomeComponent},
-  {path:'blogpage',component:MainpageComponent}
+  {path:'blogpage',component:MainpageComponent},
+  {path:'tic',component:TicToeComponent}
 ];
 
 @NgModule({
--- y/src/app/tic-toe/tic-toe.component.ts
+++ y/src/app/tic-toe/tic-toe.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tic-toe.component.scss']
 })
 export class TicToeComponent implements OnInit {
-
-  constructor() { }
-
+  public player1="1";
+  public player2="2";
+  public start=true;
+  public multi:number[][] = [[0,0,0],[0,0,0],[0,0,0]]  
+  constructor() {
+    console.log(this.multi)
+   }
   ngOnInit(): void {
+  }
+  public player: any;
+  public count=0
+  action(x:any,y:any) {
+    if(this.start)
+    this.player=this.player1
+    else
+    this.player=this.player2
+    this.multi[x][y]=this.player
+    console.log('multi',this.multi)
+    this.start=!this.start
+    this.count++;
+    if(this.count>=3) {
+      for (let i = 0; i < 3; i++) {
+        if((this.multi[i][0]==this.multi[i][1]) && (this.multi[i][2]==this.multi[i][1]) && (this.multi[i][0]==this.multi[i][2]) && this.multi[i][0]!=0) {
+          console.log("winner row",this.player);
+        }
+      }
+      for (let i = 0; i < 3; i++) {
+        if((this.multi[0][i]==this.multi[1][i]) && (this.multi[0][i]==this.multi[2][i]) && (this.multi[1][i]==this.multi[2][i]) && this.multi[0][i]!=0) {
+          console.log("winner col",this.player);
+        }
+      }
+      if((this.multi[0][0]==this.multi[1][1]) && (this.multi[0][0]==this.multi[2][2]) && (this.multi[1][1]==this.multi[2][2]) && this.multi[0][0]!=0 && this.multi[1][1]!=0 && this.multi[2][2]!=0) {
+        console.log("winner diag",this.player);
+      }
+    }
+
+    
   }
 
 }
