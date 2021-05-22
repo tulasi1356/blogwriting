1bf879345c4ab3b6371326b2c31e329a6472ec4e
tulasi1356 <botchatulasi1356@gmail.com>
2021-05-22 11:06:52 +0530

    adding another diagnol condition

--- y/src/app/tic-toe/tic-toe.component.ts
+++ y/src/app/tic-toe/tic-toe.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tic-toe.component.scss']
 })
 export class TicToeComponent implements OnInit {
-  public player1="1";
-  public player2="2";
+  public player1="TULASI";
+  public player2="Dileep";
   public start=true;
   public gameend=false;
   public multi:number[][] = [[0,0,0],[0,0,0],[0,0,0]]  
@@ -43,19 +43,31 @@ export class TicToeComponent implements OnInit {
         for (let i = 0; i < 3; i++) {
           if((this.multi[i][0]==this.multi[i][1]) && (this.multi[i][2]==this.multi[i][1]) && (this.multi[i][0]==this.multi[i][2]) && this.multi[i][0]!=0) {
             // console.log("winner row",this.player);
+            this.player="winner is"+this.player
             this.gameend=true
           }
         }
         for (let i = 0; i < 3; i++) {
           if((this.multi[0][i]==this.multi[1][i]) && (this.multi[0][i]==this.multi[2][i]) && (this.multi[1][i]==this.multi[2][i]) && this.multi[0][i]!=0) {
             // console.log("winner col",this.player);
+            this.player="winner is"+this.player
             this.gameend=true
           }
         }
         if((this.multi[0][0]==this.multi[1][1]) && (this.multi[0][0]==this.multi[2][2]) && (this.multi[1][1]==this.multi[2][2]) && this.multi[0][0]!=0 && this.multi[1][1]!=0 && this.multi[2][2]!=0) {
           // console.log("winner diag",this.player);
+          this.player="winner is"+this.player
           this.gameend=true
         }
+        if((this.multi[0][2]==this.multi[1][1]) && (this.multi[0][2]==this.multi[2][0]) && (this.multi[1][1]==this.multi[2][0]) && this.multi[0][2]!=0 && this.multi[1][1]!=0 && this.multi[2][0]!=0) {
+          // console.log("winner diag",this.player);
+          this.player="winner is"+this.player
+          this.gameend=true
+        }
+      }
+      if(this.count==9) {
+        this.player="Both of loss the game"
+        this.gameend=true
       }
     }
 
